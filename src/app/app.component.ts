f5c1d339aa54a439a7ae0005c8705d793c022c16
chinosoft <chinosoft@gmail.com>
2020-04-18 14:18:35 -0300

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-links-votes-app';
+  
+  addLink(){
+    console.log("sendind sata");
+    return false;
+  }
 }
